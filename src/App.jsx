190766809e68e123526a69c84a1c769a24648e5d
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-09-19 04:05:34 +0300

    add lighte 3D background effect,  add persistData function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-//Next is to keep the data at the reboot
-import { useState } from 'react';
+//TODO : * edite the todo item at its current location, * do some jest testing, * add the date and time dynamcly
+import { useState, useEffect } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
@@ -7,8 +7,12 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodosValue] = useState('');
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }));
+  }
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -16,6 +20,7 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     });
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -24,6 +29,20 @@ function App() {
     setTodosValue(valueToBeEdited); // Set it as the current input value
     handleDeleteTodo(index); // Optionally delete the old todo
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem('todos');
+    if (!localTodos) {
+      return;
+    }
+
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
 
   return (
     <>
